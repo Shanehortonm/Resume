d346d4acfeb5e76098d6613ed47b5651e1dbc0bb
Shane Horton <shanehortonm@gmail.com>
2022-07-16 08:26:16 -0400

    changes to javascript

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-window.onload = function(){
+ window.onload = function(){
     let name = prompt("Hey There! What's your name?");
     confirm("Just double checking, your name is " + name + " right?")
     let userName = name
 
-    document.getElementById('jsName').innerHTML = userName;
+
+    document.getElementById('jsName').innerHTML = userName + "!";
 };
