3b25594a7e0f3704f8e81bfa0e2d3267b0fb59c7
Shane Horton <shanehortonm@gmail.com>
2022-07-17 14:58:55 -0400

    editing font sizes and visual appeal

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
     let name = prompt("Hey There! What's your name?");
     confirm("Just double checking, your name is " + name + " right?")
     let userName = name
-
-
+    
     document.getElementById('jsName').innerHTML = userName + "!";
 };
