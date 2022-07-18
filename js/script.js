e7598f292bbd37eb37605af77e2f0f7a32a5c4d5
Shane Horton <shanehortonm@gmail.com>
2022-07-17 22:05:57 -0400

    changes to javascript

--- js/script.js
+++ js/script.js
@@ -2,6 +2,24 @@
     let name = prompt("Hey There! What's your name?");
     confirm("Just double checking, your name is " + name + " right?")
     let userName = name
+
+
+    
     
     document.getElementById('jsName').innerHTML = userName + "!";
 };
+
+
+/*let userName = prompt ('Yo What your name is?');
+confirm ('Just making sure your name is ' + userName + ('?'));
+alert ('Welcome ' + userName + ('!'));
+
+/*if (userName = null) {
+    prompt ('Yo foreal what your name is?');
+} else {
+    confirm ('Just making sure your name is ' + userName + ('?'));
+alert ('Welcome ' + userName + ('!'));
+}
+
+
+document.getElementById('jsName').innerHTML = userName + '!';*/
