8eb7a5887109ed668f81d0e918390e7f4c9f306a
Shane Horton <shanehortonm@gmail.com>
2022-07-19 15:35:13 -0400

    changes to javascript

--- js/script.js
+++ js/script.js
@@ -1,13 +1,63 @@
- window.onload = function(){
+window.onload = function(){
     let name = prompt("Hey There! What's your name?");
-    confirm("Just double checking, your name is " + name + " right?")
-    let userName = name
+    let userName = name;
 
+    if (userName == null || userName == '') {
+        prompt('Come on, Whats your name?')
+        if(userName == null || userName == '') {
+            prompt('Okay then, you do not have too tell me if you do not want too')
+        } else {
+            
+        }
+    } else {
+        let userNameConfirm = confirm('Just making sure your name is ' + userName + '?')
+        if (userNameConfirm == null) {
+            userName = 'Guest'
+        }
+        alert('Thanks for stopping by ' + userName)
+    }
 
-    
-    
+    if (userName == null) {
+        userName = 'Guest';
+    }
+
     document.getElementById('jsName').innerHTML = userName + "!";
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*let userName = prompt ('Yo What your name is?');
@@ -15,7 +65,7 @@ confirm ('Just making sure your name is ' + userName + ('?'));
 alert ('Welcome ' + userName + ('!'));
 
 /*if (userName = null) {
-    prompt ('Yo foreal what your name is?');
+    prompt ('Come on now, what is your name?');
 } else {
     confirm ('Just making sure your name is ' + userName + ('?'));
 alert ('Welcome ' + userName + ('!'));
@@ -25,7 +75,7 @@ alert ('Welcome ' + userName + ('!'));
 document.getElementById('jsName').innerHTML = userName + '!';*/
 
 
-const contactName = document.getElementById('contact-name');
+/*const contactName = document.getElementById('contact-name');
 const contactEmail = document.getElementById('contact-email');
 const contactNumber = document.getElementById('contact-number');
 const contactMessage = document.getElementById('contact-message');
@@ -37,4 +87,4 @@ form.addEventListener('submit', (e) => {
     if (contactName.value === '' ||  contactName.value === null) {
         messages.push('Name is required')
     }
-})
+})*/
