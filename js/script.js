75d26dcab84ec6d8b1d6df950cf0880360184ed2
Shane Horton <shanehortonm@gmail.com>
2022-07-27 18:01:57 -0400

    changes to backtotop btn

--- js/script.js
+++ js/script.js
@@ -43,8 +43,13 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        toTopBtn.style.display = 'block';   
+        toTopBtn.style.display = "block";   
     } else {
-        toTopBtn.style.display = 'none'
+        toTopBtn.style.display = "none";
     }
+}
+
+function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
